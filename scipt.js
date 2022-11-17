1621cbfa42a787d8166297521211ba018c246d98
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-11-17 09:03:10 -0800

    create list

--- scipt.js
+++ scipt.js
@@ -28,11 +28,14 @@ function createList(){
             const listItem = document.createElement('li');
             listItem.setAttribute('data-index', index);
             listItem.innerHTML = `
-                <span class"number">${index + 1}</span>
+                <span class="number">${index + 1}</span>
                 <div class="draggable" draggable="true">
                     <p class="person-name">${person}</p>
                     <i class="fas fa-grip-lines"></i>
                 </div>
             `;
+
+            listItems.push(listItem);
+            draggable_list.appendChild(listItem);
         });
 }
