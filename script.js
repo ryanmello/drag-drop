99d357661a0debb890c01857d848ed0f987be3cf
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2022-11-18 08:17:59 -0800

    scramble list item

--- script.js
+++ script.js
@@ -24,18 +24,22 @@ createList();
 // Insert list items into DOM
 function createList(){
     [...richestPeople]
-        .forEach((person, index) =>{
-            const listItem = document.createElement('li');
-            listItem.setAttribute('data-index', index);
-            listItem.innerHTML = `
-                <span class="number">${index + 1}</span>
-                <div class="draggable" draggable="true">
-                    <p class="person-name">${person}</p>
-                    <i class="fas fa-grip-lines"></i>
-                </div>
-            `;
+    .map(a => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map(a => a.value)
+    .forEach((person, index) =>{
+        console.log(person)
+        const listItem = document.createElement('li');
+        listItem.setAttribute('data-index', index);
+        listItem.innerHTML = `
+            <span class="number">${index + 1}</span>
+            <div class="draggable" draggable="true">
+                <p class="person-name">${person}</p>
+                <i class="fas fa-grip-lines"></i>
+            </div>
+        `;
 
-            listItems.push(listItem);
-            draggable_list.appendChild(listItem);
-        });
+        listItems.push(listItem);
+        draggable_list.appendChild(listItem);
+    });
 }
